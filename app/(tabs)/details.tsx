ade9ba0dba98240b14f66cbe50bef1bc0b1c74f3
Efe Polat <mefepolat@hotmail.com>
2024-10-22 18:50:47 -0400

    initial commit

---
+++ app/(tabs)/details.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Details() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Details page</Text>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
